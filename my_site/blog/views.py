921db168189cef12f26764505305c523068b9582
Qwiple <qwiple51@gmail.com>
2023-03-12 21:21:29 +0500

    profile & comments

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, redirect
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.name = request.user
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'comment_form': comment_form})
 
 def post_new(request):
     if request.method == 'POST':
@@ -37,3 +54,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def view_profile(request):
+    posts = Post.objects.filter(author = request.user)
+    return render(request, 'blog/profile.html', {'posts': posts})
+
+
