e90b6aab5ef341600a62220032f98cbfa3e65b5f
Qwiple <qwiple51@gmail.com>
2023-03-13 00:07:14 +0500

    edit & published date

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -48,7 +48,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.edit_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
